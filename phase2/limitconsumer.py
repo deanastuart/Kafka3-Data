5676e2d3bdb38b9a1a1889d4532af899767fa0f5
deanastuart <deana.stuart16@gmail.com>
2020-12-17 15:30:51 -0500

    phase 2 consumerlimit complete

---
+++ phase2/limitconsumer.py
@@ -0,0 +1,62 @@
+from kafka import KafkaConsumer, TopicPartition
+from json import loads
+from sqlalchemy import create_engine, Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+import os
+
+Base = declarative_base()
+
+
+class Transaction(Base):
+    __tablename__ = 'transaction'
+    # Here we define columns for the table person
+    # Notice that each column is also a normal Python instance attribute.
+    id = Column(Integer, primary_key=True)
+    custid = Column(Integer)
+    type = Column(String(250), nullable=False)
+    date = Column(Integer)
+    amt = Column(Integer)
+
+class XactionConsumer:
+    def __init__(self):
+        self.consumer = KafkaConsumer('bank-customer-events',
+            bootstrap_servers=['localhost:9092'],
+            # auto_offset_reset='earliest',
+            value_deserializer=lambda m: loads(m.decode('ascii')))
+        ## These are two python dictionarys
+        # Ledger is the one where all the transaction get posted
+        self.ledger = {}
+        # custBalances is the one where the current blance of each customer
+        # account is kept.
+        self.custBalances = {}
+        self.limit = -5000
+        # THE PROBLEM is every time we re-run the Consumer, ALL our customer
+        # data gets lost!
+        # add a way to connect to your database here.
+
+    def handleMessages(self):
+        for message in self.consumer:
+            message = message.value
+            print('{} received'.format(message))
+            self.ledger[message['custid']] = message
+            if message['custid'] not in self.custBalances:
+                self.custBalances[message['custid']] = 0
+            if message['type'] == 'dep':
+                self.custBalances[message['custid']] += message['amt']
+            else:
+                self.custBalances[message['custid']] -= message['amt']
+            print(self.custBalances)
+
+            record = Transaction(custid=message['custid'], type=message['type'], date=message['date'],
+                                 amt=message['amt'])
+
+            for key in self.custBalances:
+                #print(key, self.custBalances[key])
+                if self.custBalances[key] < self.limit:
+                    print("customer" + str(key) + " is below allowed negative threshold")
+if __name__ == "__main__":
+    c = XactionConsumer()
+    c.handleMessages()
+
+
